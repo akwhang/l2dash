3536c01085825ae21b14b7888fc0d5f842371233
Andrew Whang <56315025+akwhang@users.noreply.github.com>
2021-04-14 20:22:35 -0500

    Add files via upload

--- L2 Dashboard.R
+++ L2 Dashboard.R
@@ -20,7 +20,7 @@ teams <- read_csv("analyst teams.csv") %>%
   select(Name, Team)
 
 ## SVC REPORT LOAD
-svc_report <- read_csv("report1612907584937.csv") %>%
+svc_report <- read_csv("report1618431548373.csv") %>%
   slice(1:(n()-5)) %>%
   rename(svc = 'Service Timecard: Service Timecard ID',
          analyst = 'Service Timecard: Owner Name',
@@ -32,9 +32,19 @@ svc_report <- read_csv("report1612907584937.csv") %>%
          l2 = 'Assigned To') %>%
   left_join(teams, by= c("analyst" = "Name")) %>%
   filter(!is.na(Team)) %>%
-  mutate(date = mdy(date))
+  mutate(date = mdy(date),
+         date = floor_date(date, "month"))
+
 ## CASE REPORT LOAD
-case_raw <- read_csv('report1612909073254.csv')
+case_raw <- read_csv('report1618431706451.csv') %>%
+  slice(1:(n()-5)) %>%
+  rename(analyst = 'Case Owner',
+         date = 'Opened Date') %>%
+  mutate(date = mdy(date),
+         date = floor_date(date, "month")) %>%
+  filter(date > '2020-07-31')%>%
+  semi_join(teams, by=c("analyst" = "Name")) %>%
+  left_join(teams, by=c("analyst" = "Name"))
 
 ###### Functions 
 dummy <- "All"
@@ -42,27 +52,19 @@ totalRatio <- function(filter_var) {
 
   svc_total_all <- svc_report %>% 
     filter(Team == filter_var | filter_var == dummy) %>%
-    mutate(month = month(date),
-           year = year(date)) %>%
-    group_by(month) %>%
-    mutate(svc_total = n()) %>%
-    select(year, month, svc_total) %>%
-    distinct() 
+    group_by(date) %>%
+    mutate(svc_total = n()) %>% 
+    select(date, svc_total) %>% 
+    distinct() %>% 
+    arrange(date) 
   
   case_total_all <- case_raw %>%
-    slice(1:(n()-5)) %>%
-    rename(analyst = 'Case Owner',
-           date = 'Opened Date') %>%
-    mutate(date = mdy(date)) %>%
-    filter(date > firstDay, date < endDate) %>%
-    semi_join(teams, by=c("analyst" = "Name")) %>%
-    left_join(teams, by=c("analyst" = "Name")) %>%
     filter(Team == filter_var | filter_var == dummy) %>%
-    mutate(month = month(date)) %>%
-    group_by(month) %>%
-    mutate(case_total = n()) %>%
-    select(month, case_total) %>%
-    distinct() 
+    group_by(date) %>%
+    mutate(case_total = n()) %>% 
+    select(date, case_total) %>% 
+    distinct() %>% 
+    arrange(date)
   
   svccase_ratio_all <- case_total_all %>%
     left_join(svc_total_all) %>% 
@@ -70,89 +72,80 @@ totalRatio <- function(filter_var) {
     arrange(desc(ratio)) %>%
     rename("Total Cases" = "case_total",
            "Total SVCs" = "svc_total") %>%
-    arrange(month) 
-  
-  svccase_ratio_all
+    arrange(date) %>%
+    mutate(date = format(date, "%m/%Y"))
 }
 
 #plot totalRatio
 totalRatioPlot <- function(filter_var) {
+  svc_total_all <- svc_report %>% 
+    filter(Team == filter_var | filter_var == dummy) %>%
+    group_by(date) %>%
+    mutate(svc_total = n()) %>% 
+    select(date, svc_total) %>% 
+    distinct() %>% 
+    arrange(date) 
   
-  p <- ggplot(totalRatio(filter_var), aes(month, ratio)) + 
+  case_total_all <- case_raw %>%
+    filter(Team == filter_var | filter_var == dummy) %>%
+    group_by(date) %>%
+    mutate(case_total = n()) %>% 
+    select(date, case_total) %>% 
+    distinct() %>% 
+    arrange(date)
+  
+  svccase_ratio_all <- case_total_all %>%
+    left_join(svc_total_all) %>% 
+    mutate(ratio = round(svc_total/case_total*100, digits=3)) %>%
+    arrange(desc(ratio)) %>%
+    rename("Total Cases" = "case_total",
+           "Total SVCs" = "svc_total") %>%
+    arrange(date)
+  
+  p <- ggplot(svccase_ratio_all, aes(date, ratio)) + 
     geom_bar(stat="identity",aes(fill=ratio)) +
-    scale_x_continuous(breaks=c(1,2,3,4,5,6,7,8,9,10,11,12)) +
-    labs(x="month") +
-    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=3),legend.position = "none")
+    scale_x_date(date_labels = "%m-%Y",date_breaks = "1 month", name = "Month") +
+    theme(axis.text.x = element_text(angle = 60, vjust = 0.5, hjust=1),legend.position = "none")
+  
   ggplotly(p)
 }
 
 avgRatio <- function(filter_var) {
   svc_total_all <- svc_report %>% 
     filter(Team == filter_var | filter_var == dummy) %>%
-    mutate(month = month(date),
-           year = year(date)) %>%
-    group_by(month) %>%
-    mutate(svc_total = n()) %>%
-    select(month, svc_total) %>%
-    distinct()
+    group_by(date) %>%
+    mutate(svc_total = n()) %>% 
+    select(date, svc_total) %>% 
+    distinct() %>% 
+    arrange(date) 
   
   case_total_all <- case_raw %>%
-    slice(1:(n()-5)) %>%
-    rename(analyst = 'Case Owner',
-           date = 'Opened Date') %>%
-    mutate(date = mdy(date)) %>%
-    filter(date > firstDay, date < endDate) %>%
-    semi_join(teams, by=c("analyst" = "Name")) %>%
-    left_join(teams, by=c("analyst" = "Name")) %>%
     filter(Team == filter_var | filter_var == dummy) %>%
-    mutate(month = month(date)) %>%
-    group_by(month) %>%
-    mutate(case_total = n()) %>%
-    select(month, case_total) %>%
-    distinct() 
+    group_by(date) %>%
+    mutate(case_total = n()) %>% 
+    select(date, case_total) %>% 
+    distinct() %>% 
+    arrange(date)
   
-  svccase_ratio_all <- case_total_all %>%
+  avg_ratio_all <- case_total_all %>%
     left_join(svc_total_all) %>% 
     mutate(ratio = round(svc_total/case_total*100, digits=3)) %>%
     arrange(desc(ratio)) %>%
     rename("Total Cases" = "case_total",
            "Total SVCs" = "svc_total") %>%
-    arrange(month) %>%
+    arrange(date) %>%
     ungroup() %>%
     summarise(across(everything(),mean)) %>%
-    mutate(month = "") %>%
-    rename(Average = month)
-  
-  svccase_ratio_all
+    mutate(date = "") %>%
+    rename(Average = date)
 }
 
- # count by Subresolution by month
-resBreakdown <- function(filter_var) {
-  svc_total_by_subresolution <- svc_report %>% 
-    filter(Team == filter_var | filter_var == dummy) %>%
-    mutate(month = month(date),
-           subresolution = case_when(is.na(subresolution) ~ "Not Coded",
-                                     is.na(resolution) ~ "Not Coded",
-                                     TRUE ~ subresolution)) %>% 
-    group_by(resolution, subresolution, month) %>%
-    summarise(total = n()) %>%
-    spread(month, total) %>%
-    mutate(across(everything(), ~replace_na(.x, 0))) %>%
-    mutate(Total = sum(c_across(where(is.numeric)))) %>%
-    mutate(across(everything(), ~replace_na(.x, 0))) %>%
-    arrange(desc(Total)) %>%
-    filter(resolution != "NA", subresolution != "Not Coded")
-    
-}
-
+#top resolution plot
 topResLastMonth <- function(filter_var) {
   topRes <- svc_report %>% 
-    mutate(month = month(date),
-         year = year(date)) %>%
     filter((Team == filter_var | filter_var == dummy),
            !is.na(subresolution),
-           year == max(year),
-           month == max(month)) %>%
+           date == max(date)) %>%
     group_by(subresolution) %>%
     mutate(total = n()) %>%
     select(subresolution, total) %>%
@@ -168,3 +161,24 @@ topResLastMonth <- function(filter_var) {
     theme(legend.position = "none")
   ggplotly(p)
 }
+
+ # count by Subresolution by month
+resBreakdown <- function(filter_var) {
+  svc_total_by_subresolution <- svc_report %>% 
+    filter(Team == filter_var | filter_var == dummy) %>%
+    mutate(subresolution = case_when(is.na(subresolution) ~ "Not Coded",
+                                     is.na(resolution) ~ "Not Coded",
+                                     TRUE ~ subresolution)) %>% 
+    group_by(resolution, subresolution, date) %>%
+    summarise(total = n()) %>%
+    mutate(date = format(date, "%Y-%m")) %>% 
+    #arrange(date) %>%
+    spread(date, total) %>%
+    mutate(across(everything(), ~replace_na(.x, 0))) %>%
+    mutate(Total = sum(c_across(where(is.numeric)))) %>%
+    mutate(across(everything(), ~replace_na(.x, 0))) %>%
+    arrange(desc(Total)) %>%
+    filter(resolution != "NA", subresolution != "Not Coded")
+    
+}
+
